2eb11e89f1c7d64095a11a42d0ab2762fef3e8f6
sitraka <sitrakaramanoelina67@gmail.Com>
2022-05-04 10:05:41 +0100

    04/05/22

---
+++ resources/views/icon/IconTitre.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Nogae Group</title>
+    <link rel="icon" href="{{asset('img\ima.jpg')}}">
+</head>
+<body>
+    
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth/login');
 });
+
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
